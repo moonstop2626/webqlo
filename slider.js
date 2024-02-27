82f1c03b0611e6a008d4fe679fe09ac4014fa9b4
Lew Yuet Teng <ylew0006@student.monash.edu>
2024-02-28 00:35:12 +0800

    left slider

--- slider.js
+++ slider.js
@@ -1,28 +1,18 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+$(document).ready(function() {
+    let slideIndex = 0;
+    showSlides();
+  
+    function showSlides() {
+      let slides = $('.slide');
+      for (let i = 0; i < slides.length; i++) {
+        $(slides[i]).hide();
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {
+        slideIndex = 1;
+      }
+      $(slides[slideIndex - 1]).fadeIn();
+      setTimeout(showSlides, 2000); // Change slide every 2 seconds
+    }
+  });
+  
